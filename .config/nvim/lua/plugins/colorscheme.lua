59ef64f69dff181e32ae43b04f5a8de0fc9b8280
joao <joao.simsic@4x4brasil.com.br>
2026-01-21 22:32:00 -0300

    feat: cyberdream colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,14 +1,24 @@
+-- return {
+-- 	require("lazy").setup({
+--   "neanias/everforest-nvim",
+--   version = false,
+--   lazy = false,
+--   priority = 1000,
+--   config = function()
+--     require("everforest").setup({
+-- 				background = "hard",
+--     })
+-- 			vim.cmd("colorscheme everforest")
+--   end,
+-- })
+-- }
+
 return {
-	require("lazy").setup({
-  "neanias/everforest-nvim",
-  version = false,
-  lazy = false,
-  priority = 1000, 
-  config = function()
-    require("everforest").setup({
-				background = "hard",
-    })
-			vim.cmd("colorscheme everforest")
-  end,
-})
+	"scottmckendry/cyberdream.nvim",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("cyberdream").setup()
+		vim.cmd("colorscheme cyberdream")
+	end,
 }
