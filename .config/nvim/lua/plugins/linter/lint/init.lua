b92b052b7157aa47ce328278db6be34e0b1b5357
joao <joao.simsic@4x4brasil.com.br>
2026-02-01 18:05:36 -0300

    fix: linter

--- .config/nvim/lua/plugins/linter/lint/init.lua
+++ .config/nvim/lua/plugins/linter/lint/init.lua
@@ -1,24 +1,30 @@
 return {
-  "mfussenegger/nvim-lint",
-  event = { "BufReadPre", "BufNewFile" },
+	"mfussenegger/nvim-lint",
+	event = {
+		"BufReadPre",
+		"BufNewFile",
+	},
   keys = require("plugins.linter.lint.keys"),
-  config = function()
-    local lint = require("lint")
+  opts = {},
+	config = function()
+		local lint = require("lint")
 
-    lint.linters_by_ft = {
-      javascript = { "eslint_d" },
-      typescript = { "eslint_d" },
-      javascriptreact = { "eslint_d" },
-      typescriptreact = { "eslint_d" },
-      python = { "ruff" },
-    }
+		lint.linters_by_ft = {
+			javascript = { "eslint_d" },
+			typescript = { "eslint_d" },
+			javascriptreact = { "eslint_d" },
+			typescriptreact = { "eslint_d" },
+			svelte = { "eslint_d" },
+			python = { "pylint" },
+		}
 
-    vim.api.nvim_create_autocmd({ "BufWritePost", "BufEnter", "InsertLeave" }, {
-      group = lint.augroup,
-      callback = function()
-        lint.try_lint()
-      end,
-    })
+		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
-  end,
+		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+			group = lint_augroup,
+			callback = function()
+				lint.try_lint()
+			end,
+		})
+	end,
 }
