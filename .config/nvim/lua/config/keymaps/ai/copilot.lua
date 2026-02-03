c235696c8817d20dfe837720bd2003c9f7b678b9
joao <joao.simsic@4x4brasil.com.br>
2026-02-02 21:42:57 -0300

    refac: changing copilot key

--- .config/nvim/lua/config/keymaps/ai/copilot.lua
+++ .config/nvim/lua/config/keymaps/ai/copilot.lua
@@ -1,11 +1,11 @@
 return {
   {
-  "<C-l>",
+  "<leader>l",
     function()
       if require("copilot.suggestion").is_visible() then
         require("copilot.suggestion").accept()
       else
-        local termcode = vim.api.nvim_replace_termcodes("<C-l>", true, false, true)
+        local termcode = vim.api.nvim_replace_termcodes("<leader>l", true, false, true)
         vim.api.nvim_feedkeys(termcode, "n", false)
       end
     end,
