654f2693535b99a9dbd301b10d59d2b4cfed4867
joaosimsic <jpsimsic@hotmail.com>
2026-03-07 19:38:02 -0300

    refac: tmux and setup script

--- scripts/setup-dotfiles.sh
+++ scripts/setup-dotfiles.sh
@@ -2,44 +2,30 @@
 
 set -e
 
-# npm related things: neovim prettier eslint etc...
-
 DOTFILES="$HOME/dotfiles"
 
 echo "Copying dotfiles from $DOTFILES to system..."
 
-if [ ! -d "$HOME/.config" ]; then
-	echo "Creating .config directory..."
-	mkdir -p "$HOME/.config"
-fi
-
-if [ ! -d "$HOME/.tmuxifier" ]; then
-	echo "Creating .tmuxifier directory..."
-	mkdir -p "$HOME/.tmuxifier"
-fi
-
-if [ ! -d "$HOME/.local/bin" ]; then
-	echo "Creating .local/bin directory..."
-	mkdir -p "$HOME/.local/bin"
-fi
+mkdir -p "$HOME/.config"
+mkdir -p "$HOME/.tmuxifier"
+mkdir -p "$HOME/.local/bin"
 
 cp "$DOTFILES/.zshrc" "$HOME/.zshrc"
-cp "$DOTFILES/.tmux.conf" "$HOME/.tmux.conf"
+
 cp "$DOTFILES/.config/starship.toml" "$HOME/.config/"
 sudo cp "$DOTFILES/php.ini" "/etc/php/"
 
 TARGET="$HOME/.config/nvim"
-
 if [ -e "$TARGET" ] && [ ! -L "$TARGET" ]; then
     echo "Removing existing folder (not a symlink)..."
     rm -rf "$TARGET"
 fi
 
 if [ ! -L "$TARGET" ]; then
-		ln -s "$DOTFILES/.config/nvim" "$TARGET"
-    echo "Symlink created."
+    ln -s "$DOTFILES/.config/nvim" "$TARGET"
+    echo "Symlink created for Neovim."
 else
-    echo "Symlink already exists."
+    echo "Symlink already exists for Neovim."
 fi
 
 cp -r "$DOTFILES/.config/lazygit" "$HOME/.config/"
@@ -50,7 +36,6 @@ cp -r "$DOTFILES/scripts" "$HOME/.local/bin/"
 echo "Dotfiles copied successfully"
 
 echo "Changing to zsh shell"
-
 ZSH_PATH=$(which zsh)
 
 if ! grep -q "$ZSH_PATH" /etc/shells; then
@@ -59,6 +44,4 @@ if ! grep -q "$ZSH_PATH" /etc/shells; then
 fi
 
 chsh -s "$ZSH_PATH"
-
 echo "Shell changed successfully"
-
