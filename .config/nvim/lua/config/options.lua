5ab64a35cdc3fdb2fa5a400332b26721d7ca64ec
joaosimsic <jpsimsic@hotmail.com>
2026-03-07 21:21:18 -0300

    fix: removing custom d, c, y binds

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -18,16 +18,3 @@ vim.opt.termguicolors = true
 vim.opt.timeoutlen = 300
 
 vim.opt.updatetime = 250
-
-local opts = { noremap = true, silent = true }
-
-vim.keymap.set("n", "d", '"_d', opts)
-vim.keymap.set("n", "D", '"_D', opts)
-vim.keymap.set("n", "c", '"_c', opts)
-vim.keymap.set("n", "C", '"_C', opts)
-vim.keymap.set("n", "x", '"_x', opts)
-vim.keymap.set("n", "X", '"_X', opts)
-
-vim.keymap.set("v", "d", '"_d', opts)
-vim.keymap.set("v", "c", '"_c', opts)
-vim.keymap.set("v", "x", '"_x', opts)
