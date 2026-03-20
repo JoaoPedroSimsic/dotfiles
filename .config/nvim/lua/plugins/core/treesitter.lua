09e0fc010a401e5be00664d7d0d7cfdb80b60ff4
joaosimsic <joao.simsic@4x4brasil.com.br>
2026-03-20 14:26:09 -0300

    fix: treesitter

--- .config/nvim/lua/plugins/core/treesitter.lua
+++ .config/nvim/lua/plugins/core/treesitter.lua
@@ -3,63 +3,57 @@ return {
 	lazy = false,
 	build = ":TSUpdate",
 	config = function()
-		-- Try to configure blade parser (optional, may not work with newer treesitter)
-		local ok_parsers, parsers = pcall(require, "nvim-treesitter.parsers")
-		if ok_parsers and parsers.get_parser_configs then
-			local ok_config, parser_config = pcall(parsers.get_parser_configs)
-			if ok_config and parser_config then
-				parser_config.blade = {
-					install_info = {
-						url = "https://github.com/EmranMR/tree-sitter-blade",
-						files = { "src/parser.c" },
-						branch = "main",
-					},
-					filetype = "blade",
-				}
-			end
-		end
+		local parsers = require("nvim-treesitter.parsers")
 
-		local ok_configs, configs = pcall(require, "nvim-treesitter.configs")
-		if not ok_configs then
-			vim.notify("nvim-treesitter.configs not available yet", vim.log.levels.WARN)
-			return
-		end
-
-		configs.setup({
-			modules = {},
-			ignore_install = {},
-
-			ensure_installed = {
-				"php",
-				"html",
-				"blade",
-				"typescript",
-				"angular",
-				"java",
-				"go",
-				"css",
-				"lua",
-				"json",
-				"python",
-				"c_sharp",
-				"razor",
-				"markdown",
-				"markdown_inline",
+		parsers.blade = {
+			install_info = {
+				url = "https://github.com/EmranMR/tree-sitter-blade",
+				files = { "src/parser.c" },
+				branch = "main",
+				revision = "HEAD",
 			},
-			auto_install = true,
-			sync_install = false,
-			highlight = {
-				enable = true,
-				disable = function(_, buf)
-					local max_filesize = 100 * 1024
-					local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
-					if ok and stats and stats.size > max_filesize then
-						return true
-					end
-				end,
-				additional_vim_regex_highlighting = false,
+			filetype = "blade",
+			tier = "community",
+		}
+
+		vim.treesitter.language.register("blade", { "blade" })
+
+		require("nvim-treesitter").install({
+			"php",
+			"html",
+			"blade",
+			"typescript",
+			"angular",
+			"java",
+			"go",
+			"css",
+			"lua",
+			"json",
+			"python",
+			"c_sharp",
+			"razor",
+			"markdown",
+			"markdown_inline",
+		})
+
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = { "lua", "php", "blade", "javascript", "typescript", "c_sharp" },
+			callback = function(args)
+				local max_filesize = 100 * 1024
+				local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(args.buf))
+				if ok and stats and stats.size > max_filesize then
+					return
+				end
+
+				vim.treesitter.start(args.buf)
+				vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+			end,
+		})
+
+		vim.filetype.add({
+			pattern = {
+				[".*%.blade%.php"] = "blade",
 			},
-			indent = { enable = true },
 		})
 	end,
 }
