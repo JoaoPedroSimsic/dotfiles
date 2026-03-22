b0cab61c43a5ecd4d1f2270abaf36240db9b8f6b
joaosimsic <jpsimsic@hotmail.com>
2026-03-22 00:37:48 -0300

    refac: removing mason

--- .config/nvim/lua/languages/csharp.lua
+++ .config/nvim/lua/languages/csharp.lua
@@ -14,17 +14,6 @@ return {
 	{
 		"seblyng/roslyn.nvim",
 		ft = "cs",
-		dependencies = {
-			{
-				"williamboman/mason.nvim",
-				opts = {
-					registries = {
-						"github:mason-org/mason-registry",
-						"github:Crashdummyy/mason-registry",
-					},
-				},
-			},
-		},
 		config = function()
 			require("roslyn").setup({
 				args = {
--- .config/nvim/lua/plugins/core/lsp.lua
+++ .config/nvim/lua/plugins/core/lsp.lua
@@ -13,35 +13,4 @@ return {
 			end
 		end,
 	},
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup({
-				registries = {
-					"github:mason-org/mason-registry",
-					"github:Crashdummyy/mason-registry",
-				},
-			})
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		opts = {
-			ensure_installed = manifest.tools.servers,
-			automatic_enable = {
-				exclude = {
-					"jdtls",
-				},
-			},
-		},
-	},
-	{
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-		dependencies = { "williamboman/mason.nvim" },
-		opts = {
-			ensure_installed = manifest.get_tools(),
-			auto_update = true,
-			run_on_start = true,
-		},
-	},
 }
